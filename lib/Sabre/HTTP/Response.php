a127edaf1b936c5b4933efe95228bc3d10614462
Evert Pot <evert@rooftopsolutions.nl>
2013-08-15 15:13:22 +0100

    Showing my support.

--- lib/Sabre/HTTP/Response.php
+++ lib/Sabre/HTTP/Response.php
@@ -63,7 +63,8 @@ class Response extends Message implements ResponseInterface {
         426 => 'Upgrade required',
         428 => 'Precondition required', // RFC 6585
         429 => 'Too Many Requests', // RFC 6585
-        431 => 'Request Header Fields Too Large', // RFC 6585;
+        431 => 'Request Header Fields Too Large', // RFC 6585
+        451 => 'Unavailable For Legal Reasons', // draft-tbray-http-legally-restricted-status
         500 => 'Internal Server Error',
         501 => 'Not Implemented',
         502 => 'Bad Gateway',

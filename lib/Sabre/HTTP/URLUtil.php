ab49d3d97f911329b4b07070fdce2d44f4998288
Evert Pot <me@evertpot.com>
2014-07-23 20:40:17 -0400

    Added a url resolver. Much like nodejs.

--- lib/Sabre/HTTP/URLUtil.php
+++ lib/Sabre/HTTP/URLUtil.php
@@ -118,4 +118,101 @@ class URLUtil {
 
     }
 
+    /**
+     * Resolves relative urls, like a browswer would.
+     *
+     * This function takes a basePath, which itself _may_ also be relative, and
+     * then applies the relative path on top of it.
+     *
+     * @param string $basePath
+     * @param string $newPath
+     * @return void
+     */
+    static function resolve($basePath, $newPath) {
+
+        $base = parse_url($basePath);
+        $delta = parse_url($newPath);
+
+        $pick = function($part) use ($base, $delta) {
+
+            if (isset($delta[$part])) {
+                return $delta[$part];
+            } elseif (isset($base[$part])) {
+                return $base[$part];
+            } else {
+                return null;
+            }
+
+        };
+
+        $url = '';
+        $scheme = $pick('scheme');
+        $host = $pick('host');
+
+        if ($scheme) {
+            // If there's a scheme, there's also a host.
+            $url=$scheme.'://' . $host;
+        } elseif (!$scheme && $host) {
+            // No scheme, but there is a host.
+            $url = '//' . $host;
+        }
+
+        $port = $pick('port');
+        if ($port) {
+            // tcp port.
+            $url.=':' . $port;
+        }
+
+        $path = '';
+        if (isset($delta['path'])) {
+            // If the path starts with a slash
+            if ($delta['path'][0]==='/') {
+                $path = $delta['path'];
+            } else {
+                // Removing last component from base path.
+                $path = $base['path'];
+                if (strpos($path, '/')!==false) {
+                    $path = substr($path,0,strrpos($path,'/'));
+                }
+                $path.='/' . $delta['path'];
+            }
+        } else {
+            $path = isset($base['path'])?$base['path']:'/';
+        }
+        // Removing .. and .
+        $pathParts = explode('/', $path);
+        $newPathParts = [];
+        foreach($pathParts as $pathPart) {
+
+            switch($pathPart) {
+                case '' :
+                case '.' :
+                    break;
+                case '..' :
+                    array_pop($newPathParts);
+                    break;
+                default :
+                    $newPathParts[] = $pathPart;
+                    break;
+            }
+        }
+
+        $url.='/' . implode('/', $newPathParts);
+
+        // If the source url ended with a /, we want to preserve that.
+        if (substr($path, -1) === '/' && $path!=='/') {
+            $url.='/';
+        }
+
+        if (isset($delta['query'])) {
+            $url.='?' . $delta['query'];
+        }
+        if (isset($delta['fragment'])) {
+            $url.='#' . $delta['fragment'];
+        }
+
+        return $url;
+
+    }
+
 }

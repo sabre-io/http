e893b01704cd45c97ed96feb5a95322366b9a99b
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2014-11-06 15:18:10 +0100

    Add tests for invalid days and hours.

--- tests/HTTP/UtilTest.php
+++ tests/HTTP/UtilTest.php
@@ -33,6 +33,16 @@ class UtilTest extends \PHPUnit_Framework_TestCase {
             'Wednesday, 13-Oct-10 10:26:00 UTC',
             // No space before the 6
             'Wed Oct 6 10:26:00 2010',
+            // Invalid day
+            'Wed Oct  0 10:26:00 2010',
+            'Wed Oct 32 10:26:00 2010',
+            'Wed, 0 Oct 2010 10:26:00 GMT',
+            'Wed, 32 Oct 2010 10:26:00 GMT',
+            'Wednesday, 32-Oct-10 10:26:00 GMT',
+            // Invalid hour
+            'Wed, 13 Oct 2010 24:26:00 GMT',
+            'Wednesday, 13-Oct-10 24:26:00 GMT',
+            'Wed Oct 13 24:26:00 2010',
         );
 
         foreach($times as $time) {

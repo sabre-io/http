270f9f441020cf147e84803c8b924f293ccfb344
Evert Pot <me@evertpot.com>
2015-05-19 11:35:23 +0300

    CS fixes for examples/

--- examples/asyncclient.php
+++ examples/asyncclient.php
@@ -10,11 +10,8 @@
  * @author Evert Pot (http://evertpot.com/)
  * @license http://sabre.io/license/ Modified BSD License
  */
-
-use
-    Sabre\HTTP\Request,
-    Sabre\HTTP\Client;
-
+use Sabre\HTTP\Request;
+use Sabre\HTTP\Client;
 
 // Find the autoloader
 $paths = [

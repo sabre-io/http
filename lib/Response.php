be3b7f4356702d103803e019ef89fe80d8b28191
Phil Davis <phil@jankaritech.com>
2022-07-15 18:19:35 +0545

    Fix typo in text of status code 203 'Non-Authoritative Information'

--- lib/Response.php
+++ lib/Response.php
@@ -25,7 +25,7 @@ class Response extends Message implements ResponseInterface
         200 => 'OK',
         201 => 'Created',
         202 => 'Accepted',
-        203 => 'Non-Authorative Information',
+        203 => 'Non-Authoritative Information',
         204 => 'No Content',
         205 => 'Reset Content',
         206 => 'Partial Content',
